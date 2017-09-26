6f2f6c49154ac51faec2665d598e03af4774580f
vwbaker <toriiii37@gmail.com>
2017-09-26 00:36:27 -0700

    Create an initial world with the ability to move on a 20x20 ground plane

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# FindingDarwin
+README
+
+All features working
+
+I created an outdoor setting with the sun high in the sky, but a little behind and to the right of the camera, such as the one here: http://www.kleinslandscaping.com/Synthetic_Turf/Photos/Playground_Grass/outdoor%20playground%20grass.jpg.  
+
+
+
+
---
+++ resources/simple_vert.glsl
@@ -0,0 +1,19 @@
+#version  330 core
+layout(location = 0) in vec4 vertPos;
+layout(location = 1) in vec3 vertNor;
+layout(location = 2) in vec3 vertTex;
+uniform mat4 P;
+uniform mat4 M;
+uniform mat4 V;
+out vec3 fragNor;
+out vec3 WPos;
+out vec3 fragTex;
+out vec3 Dlight;
+
+void main()
+{
+	gl_Position = P * V * M * vertPos;
+	fragNor = (V * M * vec4(vertNor, 0.0)).xyz;
+	WPos = vec3(V * M * vertPos);
+	Dlight = (V * M * vec4(-1, 3, 1, 0)).xyz;
+}
